0ffae23acfadd3a0d8f8551e1b1ef6af476b6725
brittshroyer <britt.shroyer@gmail.com>
2017-02-28 13:35:19 -0600

    +hero

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,13 @@ class About extends Component{
         When children of aging parents notice a gradual loss of independence and begin to feel uncomfortable leaving them alone they usually decide to move them to assisted living.
         this decision can be fraught with buyers remorse if you aren't aware of what the facility CAN'T do.  These facilities are NOT nursing homes with fancy furniture.  If your parent is in very fragile health and seems to be declining, consider that assisted living facilities are considered NON medical facilities and not required by law to have a Registered Nurse on site.  And even if they do, they CANNOT provide hands on skilled care.
         </p>
+        <div className="hero">
+          <p>As health care consumers grow increasingly frustrated with our fragmented Health Care system, family care givers have been forced to take on much greater roles in caring for their aged parents and loved ones.
+          Overwhelmed Doctors seldom take the time to partner with families; time that could minimize hospitalizations and or institutionalization.  A recent article in my local paper pointed out an uptick in emergency calls to assisted living facilities and private homes of elderly residents. I believe that many of these calls could be avoided with ongoing comprehensive nursing visits for the purpose of identifying impending problems and intervening before a situations becomes urgent or life threatening
+          I can bridge the gaps in healthcare and help families maximize the quality of life of individuals with simple or complicated acute and chronic illnesses, all in the privacy of their home or assisted facility.
+          </p>
+          <p>My mission is to enhance the lives of the elderly by delivering professional nursing care and advocacy services that allow them to maintain an independent dignified lifestyle.</p>
+        </div>
       </div>
     );
   }
