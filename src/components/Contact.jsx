2f12ee4f3612d6c41a83b65ea39bc178a7e1339f
brittshroyer <britt.shroyer@gmail.com>
2017-03-03 23:34:26 -0600

    +responsive message area

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,28 +4,37 @@ import React, {Component} from 'react';
 class Contact extends Component{
   render(){
     return(
-      <div>
-        <div className="infoContainer">
+      <div className="contact">
+        <div className="infoContainer col-md-6">
           <div className="myInfo">
             <h2>Get In Touch</h2>
             <i className="fa fa-phone fa-1x" aria-hidden="true"></i>&nbsp;&nbsp;<p>440-666-0070</p><br/>
             <i className="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;<p>pcshroyer@gmail.com</p>
           </div>
         </div>
-        <div className="emailForm">
-          <form method="post" action="acknowledge.php">
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name"></input>
+        <form method="post" action="acknowledge.php" className="contactSection">
+          <div className="name-email">
+            <div className="enterName">
+              <label for="name">Name:</label>
+              <input type="text" name="name" id="name" className="input"></input>
+            </div>
+            <div className="enterEmail">
+              <label for ="email">Email:</label>
+              <input type="email" name="email" id="email" className="input"></input>
+            </div>
+          </div>
+          <div className="messageSection">
+            <div className="enterMessage">
+              <label for="comments">Message:</label>
+              <textarea name="comments" id="comments" className="textBox">
+              </textarea>
+            </div>
+            <div className="send-btn">
+              <input type="submit" name="send" value="Send" className="send-msg"></input>
+            </div>
+          </div>
+        </form>
 
-            <label for ="email">Email:</label>
-            <input type="email" name="email" id="email"></input>
-
-            <label for="comments">Message:</label>
-            <textarea name="comments" id="comments"></textarea>
-
-            <input type="submit" name="send" value="Send Message"></input>
-          </form>
-        </div>
       </div>
     );
   }
