2bd105fcafcb45f589b3e34dd37a16b9f6c049d7
brittshroyer <britt.shroyer@gmail.com>
2017-03-04 22:37:52 -0600

    +approach and testimonial component boilerplates

---
+++ src/components/Approach.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Approach extends Component{
+  render(){
+    return(
+      <div>
+        Services Go Here
+      </div>
+    )
+  }
+}
+
+export default Approach;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
+import Approach from './components/Approach.jsx';
+import Testimonials from './components/Testimonials.jsx';
 
 
 ReactDOM.render(
@@ -17,6 +19,8 @@ ReactDOM.render(
       <IndexRoute component={Home}/>
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
+      <Route path="/approach" component={Approach} />
+      <Route path="/testimonials" component={Testimonials} />
     </Route>
 
   </Router>),
