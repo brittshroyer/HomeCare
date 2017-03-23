c86e6feeaacc3e1b1fe175022b3efb745d9dd993
brittshroyer <britt.shroyer@gmail.com>
2017-03-23 11:46:04 -0500

    +profile img titles

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class ProfilePic extends Component{
   render(){
     return(
-      <div className="profile-img"></div>
+      <div className="profile-img">
+        <p className="name">Patty Shroyer</p>
+        <p className="professional-title">BSN &nbsp; RN</p>
+      </div>
     )
   }
 }
