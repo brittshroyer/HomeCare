eaea3d2da857daf07f033003e89f73482af5f452
brittshroyer <britt.shroyer@gmail.com>
2017-03-23 14:02:32 -0500

    +responsive home screen layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ class Footer extends Component{
   render(){
     return(
       <div className="footer">
-        <p>Delivering professional and personalized healthcare to your loved ones.</p>
+        <p id="title">Patty Shroyer&nbsp; | &nbsp;BSN & RN</p>
+        <p>Nursing Care and Advocacy Services for Your Loved Ones</p>
+        <p id="mobile-footer-title">Patty Shroyer&nbsp; | &nbsp;BSN & RN</p>
       </div>
     )
   }
--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -4,8 +4,6 @@ class ProfilePic extends Component{
   render(){
     return(
       <div className="profile-img">
-        <p className="name">Patty Shroyer</p>
-        <p className="professional-title">BSN &nbsp; RN</p>
       </div>
     )
   }
